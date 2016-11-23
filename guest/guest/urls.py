e24d64cfe033f6c49e35398d556c6f3fdb5c8f3a
zouxu_PC <zouxu_PC>
2016-11-23 18:04:57 +0800

    添加登录会话管理

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from sign import views # 导入sign应用views文件
+from sign import views
+# 导入sign应用views文件
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^index/$', views.index), # 添加 index/路径配置
+    url(r'^login_action/$', views.login_action),
 ]
